9acb64be774e807d2985c8686ffbb77fb7c2292a
Artur Bogdanik <arturb13@protonmail.com>
2026-06-14 16:42:15 +0200

    Create console user interface menu loop

--- Quotes.Client/Program.cs
+++ Quotes.Client/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine("1. Losuj zlota mysl");
+    Console.WriteLine("2. Dodaj zlota mysl");
+    Console.WriteLine("3. Wyjdz");
+    Console.Write("Wybor: ");
+    
+    var choice = Console.ReadLine();
+    if (choice == "3") break;
+
+    Console.WriteLine("\nNacisnij dowolny klawisz...");
+    Console.ReadKey();
+}
